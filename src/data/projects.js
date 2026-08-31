bcdfc1476386e6436a44da1f31e4fe36801bfdc2
jkibura <167850144+jkibura@users.noreply.github.com>
2026-08-31 20:04:38 +0300

    Put tags for each project

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,9 +5,9 @@ import haborCleaning from '../assets/images/haborcitycleaning.jpg'
 import scriptIQ from '../assets/images/script-iq.jpeg'
 
 const projects = [
-    {id: 1, name: 'Clinical Psychologist Website', description: 'A professional brand website for a Clinical Psychologist', image: sarahPsyd, link: 'https://psyd-two.vercel.app/', tags: ['tag1', 'tag2', 'tag3']},
-    {id: 2, name: 'Habor City Elite Cleaning', description: 'A professional brand website for a Premiun cleaning company', image: haborCleaning, link: 'https://cleaning-4.onrender.com/', tags: ['tag1', 'tag2', 'tag3']},
-    {id: 3, name: 'ScriptIQ', description: 'A custom brand and ecommerce website for a fasr growing premium notebook brand in Kenya', image: scriptIQ, link: 'https://script-iq-alpha.vercel.app/', tags: ['tag1', 'tag2', 'tag3']},
+    {id: 1, name: 'Clinical Psychologist Website', description: 'A professional brand website for a Clinical Psychologist', image: sarahPsyd, link: 'https://psyd-two.vercel.app/', tags: ['HTML', 'CSS', 'Javascript']},
+    {id: 2, name: 'Habor City Elite Cleaning', description: 'A professional brand website for a Premiun cleaning company', image: haborCleaning, link: 'https://cleaning-4.onrender.com/', tags: ['React', 'Typescript', 'CSS']},
+    {id: 3, name: 'ScriptIQ', description: 'A custom brand and ecommerce website for a fasr growing premium notebook brand in Kenya', image: scriptIQ, link: 'https://script-iq-alpha.vercel.app/', tags: ['React', 'Typescript', 'CSS']},
 ]
 
 export default  projects
