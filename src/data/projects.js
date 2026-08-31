439cb2c3cc03bd50a5e9723169f9bfdeda876bd7
jkibura <167850144+jkibura@users.noreply.github.com>
2026-08-31 20:00:08 +0300

    Updated the projects, icons and descriptions. Yet to add customize the image formats.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,13 @@
-import aboutImage from '../assets/images/about.png'
-import projectImage from '../assets/images/image.png'
+// import aboutImage from '../assets/images/about.png'
+// import projectImage from '../assets/images/image.png'
+import sarahPsyd from '../assets/images/sarahpsychologist.jpg'
+import haborCleaning from '../assets/images/haborcitycleaning.jpg'
+import scriptIQ from '../assets/images/script-iq.jpeg'
 
 const projects = [
-    {id: 1, name: 'Project 1', description: 'Description 1', image: aboutImage, link: '', tags: ['tag1', 'tag2', 'tag3']},
-    {id: 2, name: 'Project 2', description: 'Description 2', image: projectImage, link: '', tags: ['tag1', 'tag2', 'tag3']},
-    {id: 3, name: 'Project 3', description: 'Description 3', image: aboutImage, link: '', tags: ['tag1', 'tag2', 'tag3']},
+    {id: 1, name: 'Clinical Psychologist Website', description: 'A professional brand website for a Clinical Psychologist', image: sarahPsyd, link: 'https://psyd-two.vercel.app/', tags: ['tag1', 'tag2', 'tag3']},
+    {id: 2, name: 'Habor City Elite Cleaning', description: 'A professional brand website for a Premiun cleaning company', image: haborCleaning, link: 'https://cleaning-4.onrender.com/', tags: ['tag1', 'tag2', 'tag3']},
+    {id: 3, name: 'ScriptIQ', description: 'A custom brand and ecommerce website for a fasr growing premium notebook brand in Kenya', image: scriptIQ, link: 'https://script-iq-alpha.vercel.app/', tags: ['tag1', 'tag2', 'tag3']},
 ]
 
 export default  projects
